e3d78d86ee5f3b557af66fb3512ad79bf3daace9
Bharathajxworkz <bharathaj2211.xwokz@gmail.com>
2021-12-27 13:00:37 +0530

    the out put has to print in two line

---
+++ Bikes.java
@@ -0,0 +1,17 @@
+class Bikes{
+	public static void main(String []index){
+	System.out.println("Start the main()\t");
+	
+		for(int bikeNames=0;bikeNames<5;bikeNames++){
+				System.out.print("\t" +index[bikeNames]);
+		}
+	System.out.println();
+	
+		for(int bikeNames=5;bikeNames<10;bikeNames++){
+			System.out.print("\t" +index[bikeNames]);
+		
+		}
+	System.out.println();
+	System.out.println("End of the main()");
+	}
+}
