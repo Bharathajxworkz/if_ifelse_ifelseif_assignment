3bbf57b66a507e558976a0afe3dcc5b68ac3fd4f
Bharathajxworkz <bharathaj2211.xwokz@gmail.com>
2021-12-27 12:58:57 +0530

    string agrguments is program the input are given in run time

---
+++ Car.java
@@ -0,0 +1,27 @@
+class Car{
+	public static void main(String[] bharath){
+
+		System.out.println("The main method start");
+		
+		for(int index = 0;index < bharath.length;index++){
+		
+		System.out.println(bharath[index]);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		}
+	
+	System.out.println ("The main method end");
+	
+	
+	}
+
+
+
+}
